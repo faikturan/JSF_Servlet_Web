10dbf6c897a23f8dde06927af1865761a1cf22f5
Faik TURAN <faikturan@users.noreply.github.com>
2017-05-09 20:10:01 +0300

    Add files via upload

---
+++ User.java
@@ -0,0 +1,122 @@
+package com.faikturan.hastaneotomasyonu.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.sun.istack.internal.NotNull;
+
+
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String firstName;
+	private String lastName;
+	private String identityNumber;
+	private String maleFemale;
+	private String birthPlace;
+	private String birthDate;
+	private String fatherName;
+	private String motherName;
+	private String phoneNumber;
+	private String homeNumber;
+	private String emailAddress;
+	private String emailAddressRepeat;
+	private String password;
+	private String passwordRepeat;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getIdentityNumber() {
+		return identityNumber;
+	}
+	public void setIdentityNumber(String identityNumber) {
+		this.identityNumber = identityNumber;
+	}
+	public String getMaleFemale() {
+		return maleFemale;
+	}
+	public void setMaleFemale(String maleFemale) {
+		this.maleFemale = maleFemale;
+	}
+	public String getBirthPlace() {
+		return birthPlace;
+	}
+	public void setBirthPlace(String birthPlace) {
+		this.birthPlace = birthPlace;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getFatherName() {
+		return fatherName;
+	}
+	public void setFatherName(String fatherName) {
+		this.fatherName = fatherName;
+	}
+	public String getMotherName() {
+		return motherName;
+	}
+	public void setMotherName(String motherName) {
+		this.motherName = motherName;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getHomeNumber() {
+		return homeNumber;
+	}
+	public void setHomeNumber(String homeNumber) {
+		this.homeNumber = homeNumber;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getEmailAddressRepeat() {
+		return emailAddressRepeat;
+	}
+	public void setEmailAddressRepeat(String emailAddressRepeat) {
+		this.emailAddressRepeat = emailAddressRepeat;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPasswordRepeat() {
+		return passwordRepeat;
+	}
+	public void setPasswordRepeat(String passwordRepeat) {
+		this.passwordRepeat = passwordRepeat;
+	}
+
+}
