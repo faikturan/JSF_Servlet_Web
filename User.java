3e79ff82c9f952742c0452dd7f4a13ad3edaff5d
Faik TURAN <faikturan@users.noreply.github.com>
2017-05-11 19:50:26 +0300

    Add files via upload

--- User.java
+++ User.java
@@ -2,23 +2,8 @@ package com.faikturan.hastaneotomasyonu.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.validation.constraints.Size;
-import javax.xml.bind.annotation.XmlRootElement;
-
-import com.sun.istack.internal.NotNull;
-
 
 public class User implements Serializable {
-	private static final long serialVersionUID = 1L;
 	
 	private String firstName;
 	private String lastName;
